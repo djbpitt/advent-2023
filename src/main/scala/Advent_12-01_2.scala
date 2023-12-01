bd1294e7da8ed147d54801a9356125c43e954109
David J Birnbaum <djbpitt@gmail.com>
2023-12-01 15:48:48 -0500

    Use Char instead of String for characters

--- src/main/scala/Advent_12-01_2.scala
+++ src/main/scala/Advent_12-01_2.scala
@@ -6,45 +6,43 @@ import scala.language.postfixOps
 /** Problem is underspecified
  * nineight (with overlap) resolves to 98 (the overlap counts for both)
  * */
-val wordToInt = Map(
-  "one" -> "1",
-  "two" -> "2",
-  "three" -> "3",
-  "four" -> "4",
-  "five" -> "5",
-  "six" -> "6",
-  "seven" -> "7",
-  "eight" -> "8",
-  "nine" -> "9"
+val wordToChar: Map[String, Char] = Map(
+  "one" -> '1',
+  "two" -> '2',
+  "three" -> '3',
+  "four" -> '4',
+  "five" -> '5',
+  "six" -> '6',
+  "seven" -> '7',
+  "eight" -> '8',
+  "nine" -> '9'
 )
-@main def main2() =
-  val digitPattern = ("^(" + wordToInt.keys.mkString("|") + ")").r
+@main def main2(): Unit =
+  val digitPattern = ("^(" + wordToChar.keys.mkString("|") + ")").r
   val input = os.read(os.pwd/"src"/"resources"/"12-01_data.txt")
     .split("\n")
   @tailrec
-  def findDigits(item: String, accumulator: Vector[String]): Vector[String] =
+  def findDigits(item: String, accumulator: Vector[Char]): Vector[Char] =
     if item.isEmpty then
       accumulator
     else
       val first = item.toCharArray.head
       item match {
-        case _ if Character.isDigit(first) =>
-          findDigits(item.substring(1), accumulator :+ first.toString)
+        case _ if first.isDigit =>
+          findDigits(item.substring(1), accumulator :+ first)
         case _ =>
           val digitAsWord = digitPattern.findAllIn(item)
           if digitAsWord.isEmpty then
             findDigits(item.substring(1), accumulator)
           else
             val word = digitAsWord.toVector.head
-            val digit = wordToInt(word)
+            val digit = wordToChar(word)
             findDigits(item.substring(1), accumulator :+ digit)
       }
   val results = input
     .map(findDigits(_, Vector()))
-    .map(e => (e.head.toInt * 10 + e.last.toInt)).sum
+    .map(e => e.head.asDigit * 10 + e.last.asDigit).sum
   println(results)
-
-
 
 
 
