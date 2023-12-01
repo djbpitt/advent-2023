4fb29c9c58b64f384c19715dfd40d5eabd5a95a8
David J Birnbaum <djbpitt@gmail.com>
2023-12-01 16:13:23 -0500

    Use CharArray instead of String as main type for findDigits()
    We spend most of our time with a CharArray, using a string only to check for initial words (if we get that far)

--- src/main/scala/Advent_12-01_2.scala
+++ src/main/scala/Advent_12-01_2.scala
@@ -1,9 +1,9 @@
 import os.*
 
 import scala.annotation.tailrec
-import scala.language.postfixOps
+import scala.language.postfixOps // needed for .r regex pattern
 
-/** Problem is underspecified
+/** Task is underspecified
  * nineight (with overlap) resolves to 98 (the overlap counts for both)
  * */
 val wordToChar: Map[String, Char] = Map(
@@ -18,30 +18,43 @@ val wordToChar: Map[String, Char] = Map(
   "nine" -> '9'
 )
 @main def main2(): Unit =
+  // Construct pattern to match number word at beginning dynamically
   val digitPattern = ("^(" + wordToChar.keys.mkString("|") + ")").r
   val input = os.read(os.pwd/"src"/"resources"/"12-01_data.txt")
     .split("\n")
   @tailrec
-  def findDigits(item: String, accumulator: Vector[Char]): Vector[Char] =
+  /** Find all numbers, whether digits or words
+   *
+   * @param item : input line as CharArray
+   * @param accumulator : holds all digits found as Vector[Char]
+   *
+   * @return Vector[Char] of all digits found
+   * */
+  def findDigits(item: Array[Char], accumulator: Vector[Char]): Vector[Char] =
     if item.isEmpty then
       accumulator
     else
-      val first = item.toCharArray.head
+      val first = item.head
       item match {
         case _ if first.isDigit =>
-          findDigits(item.substring(1), accumulator :+ first)
+          // Array.drop(n) slices to end without knowing length
+          findDigits(item.drop(1), accumulator :+ first)
         case _ =>
-          val digitAsWord = digitPattern.findAllIn(item)
+          // Need to convert CharArray to String for regex matching
+          val digitAsWord = digitPattern.findAllIn(item.mkString)
           if digitAsWord.isEmpty then
-            findDigits(item.substring(1), accumulator)
+            findDigits(item.drop(1), accumulator)
           else
+            // There must be exactly one if we get this far, and the lookup must succeed
             val word = digitAsWord.toVector.head
             val digit = wordToChar(word)
-            findDigits(item.substring(1), accumulator :+ digit)
+            findDigits(item.drop(1), accumulator :+ digit)
       }
   val results = input
+    .map(_.toCharArray) // Convert to CharArray early, since that’s where we spend the most time
     .map(findDigits(_, Vector()))
-    .map(e => e.head.asDigit * 10 + e.last.asDigit).sum
+    .map(e => e.head.asDigit * 10 + e.last.asDigit) // Treat Char as Int
+    .sum
   println(results)
 
 
