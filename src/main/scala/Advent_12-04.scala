99679d0b91cd823770f416ae1676f3a000c2ba71
David J Birnbaum <djbpitt@gmail.com>
2023-12-04 20:11:55 -0500

    12-04 part 2 multiplier works with test data

--- src/main/scala/Advent_12-04.scala
+++ src/main/scala/Advent_12-04.scala
@@ -38,26 +38,44 @@ private def scoreCard(winCount: Int): Int =
     case e => scala.math.pow(2, e - 1).toInt
   }
 
-private def main4_1(cardScores: Vector[Int]): Unit =
-  println(cardScores.sum)
-
-private def main4_2(winCounts: Vector[Int]): Unit =
-  // cardNo, winCount, score
+private def computeMultipliers(winCounts: Vector[Int], cardScores: Vector[Int]): Vector[Int] =
   @tailrec
-  def computeCardCounts(offset: Int, cardReps: Vector[Int]): Vector[Int] =
+  def computeMultiplierStep(offset: Int, cardReps: Vector[Int]): Vector[Int] =
+//    if offset == winCounts.size then cardReps
     if offset == winCounts.size then cardReps
     else
-      // Update cardReps here
-      computeCardCounts(offset + 1, cardReps)
-  val cardReps = computeCardCounts(offset = 0, cardReps = Vector.fill(winCounts.size)(1))
-  println(cardReps.size)
+      val winnersAtOffset: Int = winCounts(offset) // Determines how many games to augment
+      // If Card at position 0 has 4 winners, hit positions 1 through 5 = 0 + 1 through (exclusive) 0 + 4 + 1
+      val countsToAugment: Vector[Int] = cardReps.slice(offset + 1, offset + winnersAtOffset + 1)
+      val augmentedCounts: Vector[Int] = countsToAugment.map(_ + cardReps(offset))
+      val newCardReps: Vector[Int] = cardReps.take(offset + 1) :++ augmentedCounts :++ cardReps.drop(offset + winnersAtOffset + 1)
+      println(s"Offset: $offset, Winners: $winnersAtOffset, New reps: $newCardReps")
+      computeMultiplierStep(offset + 1, newCardReps)
+  val multipliers: Vector[Int] = computeMultiplierStep(offset = 0, cardReps = Vector.fill(winCounts.size)(1))
+  multipliers
+
+private def main4_1(cardScores: Vector[Int]): Unit =
+  val result = cardScores.sum
+  println(s"Part 1: $result")
+
+private def main4_2(winCounts: Vector[Int], cardScores: Vector[Int]): Unit =
+  val multipliers: Vector[Int] = computeMultipliers(winCounts, cardScores)
+  val result = winCounts.zip(multipliers).zip(cardScores).map(e => (e._1._1, e._1._2, e._2))
+  println(s"Number of winners | Multiplier | Card score")
+  result.foreach(println)
 
 @main def main4(): Unit =
   // Setup
-  val dataLines = os.read(os.pwd / "src" / "resources" / "12-04_data.txt")
-    .split("\n")
+  // val data = os.read(os.pwd / "src" / "resources" / "12-04_data.txt")
+  val data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+               |Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+               |Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+               |Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+               |Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+               |Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""".stripMargin
+  val dataLines = data.split("\n")
   val splitLines = dataLines.map(parseLine) // (cardNo: Int, winners: String, toCheck: String)
   val winCounts = splitLines.map(e => countWins(e._2, e._3)).toVector // number of winners as Int
   val cardScores = winCounts.map(scoreCard)
   main4_1(cardScores)
-  main4_2(winCounts)
+  main4_2(winCounts, cardScores)
