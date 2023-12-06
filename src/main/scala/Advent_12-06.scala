16d4ef30baf2cec045a2100a47a3be8ab7d9d6ad
David J Birnbaum <djbpitt@gmail.com>
2023-12-06 14:46:09 -0500

    12-06 refactor Int as Long for part 2

--- src/main/scala/Advent_12-06.scala
+++ src/main/scala/Advent_12-06.scala
@@ -1,29 +1,42 @@
 import scala.io.Source
+import scala.util.matching.Regex
 
-private def parseInput(filename: String): Vector[Race] =
+val nonDigitpattern: Regex = """\D+""".r
+private def parseInput_1(filename: String): Vector[Race] =
   val rawData = Source.fromResource(filename).mkString("")
   val dataParts = rawData.split("\n").map(_.split("\\s+"))
-  val stats = dataParts.head.zip(dataParts.last).tail.map(e => Race(e._1.toInt, e._2.toInt))
+  val stats = dataParts.head.zip(dataParts.last).tail.map(e => Race(e._1.toLong, e._2.toLong))
   stats.toVector
 
 private def computeTimeDistanceMapping(record: Race): Vector[Race] =
   val mappings =
-    for pressTime: Int <- 0 to record.time yield
-      val runTime: Int = record.time - pressTime
+    for pressTime: Long <- 0L to record.time yield
+      val runTime: Long = record.time - pressTime
       Race(pressTime, runTime * pressTime)
   mappings.filter(e => e.distance > record.distance).toVector
 
-def main6_1(stats: Vector[Race]): Unit =
+
+private def parseInput_2(filename: String): Race =
+  val rawData: String = Source.fromResource(filename).mkString("")
+  val dataParts = rawData.split("\n")
+  val time: Long = nonDigitpattern.replaceAllIn(dataParts.head, "").toLong
+  val distance: Long = nonDigitpattern.replaceAllIn(dataParts.last, "").toLong
+  Race(time, distance)
+
+
+def main6_1(): Unit =
+  val stats: Vector[Race] = parseInput_1("12-06_data.txt")
+  // val stats: Vector[Race] = parseInput_1("12-06_data_test.txt")
   val winnerCounts = stats.map(e => computeTimeDistanceMapping(e).length)
   val winnerProduct = winnerCounts.product
   println(s"Part 1: $winnerProduct")
 
-@main def main6(): Unit =
-  /* ***
-  * Setup
-  */
-  val stats: Vector[Race] = parseInput("12-06_data.txt")
-  // val stats: Vector[Race] = parseInput("12-06_data_test.txt")
-  main6_1(stats)
+def main6_2(): Unit =
+  val stats: Race = parseInput_2("12-06_data.txt")
+  println(stats)
 
-case class Race(time: Int, distance: Int)
+@main def main6(): Unit =
+  main6_1()
+  main6_2()
+
+case class Race(time: Long, distance: Long)
