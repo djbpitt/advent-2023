1929a3c1f7f3e12da877fd1230063cb1abbb8787
David J Birnbaum <djbpitt@gmail.com>
2023-12-06 11:50:35 -0500

    12-06 part 1 complete

--- src/main/scala/Advent_12-06.scala
+++ src/main/scala/Advent_12-06.scala
@@ -6,12 +6,21 @@ private def parseInput(filename: String): Vector[Race] =
   val stats = dataParts.head.zip(dataParts.last).tail.map(e => Race(e._1.toInt, e._2.toInt))
   stats.toVector
 
+private def computeTimeDistanceMapping(record: Race): Vector[Race] =
+  val mappings =
+    for pressTime: Int <- 0 to record.time yield
+      val runTime: Int = record.time - pressTime
+      Race(pressTime, runTime * pressTime)
+  mappings.filter(e => e.distance > record.distance).toVector
+
 @main def main6(): Unit =
   /* ***
   * Setup
   */
-  // val stats = parseInput("12-06_data.txt")
-  val stats = parseInput("12-06_data_test.txt")
-  println(stats)
+  val stats: Vector[Race] = parseInput("12-06_data.txt")
+  // val stats: Vector[Race] = parseInput("12-06_data_test.txt")
+  val winnerCounts = stats.map(e => computeTimeDistanceMapping(e).length)
+  val winnerProduct = winnerCounts.product
+  println(s"Part 1: $winnerProduct")
 
 case class Race(time: Int, distance: Int)
