eb83f79378b5e4e66ae119323a93cccc83f52c72
David J Birnbaum <djbpitt@gmail.com>
2023-12-09 13:39:20 -0500

    12-07 Clean up part 2 and add comments

--- src/main/scala/Advent_12-07_2.scala
+++ src/main/scala/Advent_12-07_2.scala
@@ -3,7 +3,6 @@ package Advent_12_07_2
 /** AoC 2023, Day 7, part 2
  *
  * Separate package because reuses names
- *
  */
 
 import scala.io.Source
@@ -22,7 +21,7 @@ private object Hands extends Enumeration {
 }
 import Hands._
 
-/** Add method to promote Hand type for jokers
+/** Method to promote Hand type for jokers
  *
  * Will not overrun with natural data
  *
@@ -43,14 +42,14 @@ protected case class Hands(i: Int, name: String, promote: Hand)
  */
 def promote(in: Hand, steps: Int): Hand =
   @tailrec
-  def promoteStep(hand: Hand, count: Int): Hand =
+  def promoteStep(hand: Hand, count: Int): Hand = // increase hand type by steps recursively
     if count == 0 then hand // no more jokers
-    else
-      if hand == ThreeOfAKind then // skip full house
-        promoteStep(FourOfAKind, count - 1)
-      else
-        promoteStep(Hands(hand.id + 1), count - 1)
-  val promoted: Hand = promoteStep(in: Hand, steps: Int)
+    else if hand == ThreeOfAKind then // skip full house
+      promoteStep(FourOfAKind, count - 1)
+    else // next step in order
+      promoteStep(Hands(hand.id + 1), count - 1)
+
+  val promoted: Hand = promoteStep(in: Hand, steps: Int) // launch recursion
   promoted
 
 private def parseHandData(rawHandInput: String): (String, Int) =
@@ -59,7 +58,7 @@ private def parseHandData(rawHandInput: String): (String, Int) =
     case _ => (s"ERROR: $rawHandInput", -1)
   }
 
-/** Convert Char from hand to enum card value
+/** Convert Char from hand to enum Card (Value)
  *
  * E.g., A => CA, 4 => C4
  *
@@ -69,23 +68,41 @@ private def parseHandData(rawHandInput: String): (String, Int) =
 private def cardify(input: Char): Card =
   Cards.withName("C" + input.toString)
 
+/** Map from card type to count of cards of that type
+ *
+ * Possible improvements:
+ * Add joker count of 0 where missing to simplify testing for presence of absence later
+ * Rename function (currently "handify") because it returns card counts but not hand type
+ *
+ * @param input Vector[Card] with five cards in hand
+ * @return map from card types to counts
+ */
 private def handify(input: Vector[Card]): Map[Card, Int] =
   val grouped = input
     .groupBy(identity)
     .map((key, value) => key -> value.length)
   grouped
 
+/** Determine hand type from card counts
+ *
+ * Does not yet adapt type for jokers
+ *
+ * @param input Map[Card, Int] with counts of each card type in hand
+ * @return Hand
+ */
 private def handType(input: Map[Card, Int]): Hand =
-  val counts = input.values
-  input match {
-    case h if counts.max == 5 => FiveOfAKind
-    case h if counts.max == 4 => FourOfAKind
-    case h if counts.max == 3 && input.size == 2 => FullHouse
-    case h if counts.max == 3 && input.size == 3 => ThreeOfAKind
-    case h if counts.max == 2 && input.size == 3 => TwoPair
-    case h if counts.max == 2 && input.size == 4 => OnePair
-    case h if counts.max == 5 => HighCard
-    case _ => new RuntimeException("oops"); HighCard
+  input.values.max match {
+    case 5 => FiveOfAKind
+    case 4 => FourOfAKind
+    case 3 => input.size match {
+      case 2 => FullHouse
+      case _ => ThreeOfAKind
+    }
+    case 2 => input.size match {
+      case 3 => TwoPair
+      case _ => OnePair
+    }
+    case _ => HighCard
   }
 @main def main7(): Unit =
   val rawInput: Vector[String] = Source.fromResource("12-07_data_test.txt").getLines.toVector // :+ "JJJJJ 10000"
@@ -93,16 +110,16 @@ private def handType(input: Map[Card, Int]): Hand =
   val cardsByHand: Vector[Vector[Card]] = rawCardInput.map(e => e._1.map(cardify).toVector)
   val hands: Vector[Map[Card, Int]] = cardsByHand.map(handify)
   val handTypes: Vector[Hand] = hands.map(handType)
-  val handsAndTypes = cardsByHand
+  val handsAndTypes: Vector[(Vector[Card], Hand)] = cardsByHand
     .zip(handTypes)
   val promotedTypes: Vector[Hand] = handsAndTypes.map {
-    case (cards, hand) if cards.contains(CJ) && (hand == FiveOfAKind) => FiveOfAKind
+    case (_, hand) if hand == FiveOfAKind => FiveOfAKind
     case (cards, hand) if cards.contains(CJ) =>
       val jokerCount = cards.groupBy(identity)(CJ).length
       promote(hand, jokerCount)
     case (_, hand) => hand
   }
-  val bets = rawCardInput.map((e, f) => f)
+  val bets: Vector[Int] = rawCardInput.map((_, f) => f)
   val cardDataInstances = cardsByHand
     .zip(promotedTypes)
     .zip(bets)
@@ -110,7 +127,9 @@ private def handType(input: Map[Card, Int]): Hand =
   // rank (low to high, one-based) is offset + 1
   val sortedCardDataInstances: Vector[CardData] = cardDataInstances.sortBy(e => (e.HandType, e.Cards))
   sortedCardDataInstances.foreach(println)
-  val amountWon = sortedCardDataInstances.zipWithIndex.map((data, rank) => data.Bet * (rank + 1)).sum
+  val amountWon = sortedCardDataInstances
+    .zipWithIndex
+    .map((data, rank) => data.Bet * (rank + 1)).sum
   println(s"Part 2: $amountWon")
 
 case class CardData(Cards: Vector[Card], HandType: Hand, Bet: Int)
