4fd53d1b1721dd1ceebcdbe9ce930f3b4bfd1d74
David J Birnbaum <djbpitt@gmail.com>
2023-12-10 15:02:26 -0500

    12-10 Part 1 setup
    Parse input, create cells with row, column, and data
    Find start cell neighbors

--- src/main/scala/Scratch.scala
+++ src/main/scala/Scratch.scala
@@ -1,16 +1,33 @@
 package scratch
 
-object Hands extends Enumeration {
-  type Card = Value
-  val High, Mid, Low = Value
-}
+/*
+| is a vertical pipe connecting north and south. 2503
+- is a horizontal pipe connecting east and west. 2501
+L is a 90-degree bend connecting north and east. 2517
+J is a 90-degree bend connecting north and west. 251B
+7 is a 90-degree bend connecting south and west. 2513
+F is a 90-degree bend connecting south and east. 250F
+ */
 
-import Hands._
-protected case class Hands(i: Int, name: String, promote: Value)
-  def promote(in: Value): Value =
-    val promoted = Hands(in.id + 1)
-    promoted
+val sample: String = """.....
+                       |.F-7.
+                       |.|.|.
+                       |.L-J.
+                       |.....""".stripMargin
+
+private def mapToBoxChar(char: Char): Char =
+  val mapping: Map[Char, Char] =
+    List('|' -> '┃',
+    '-' -> '━',
+    'L' -> '┗',
+    'J' -> '┛',
+    '7' -> '┓',
+    'F' -> '┏',
+    '.' -> '·',
+    '\n' -> '\n').toMap
+  mapping.getOrElse(char, ' ')
+
 
 @main def scratch(): Unit =
-  val promoted = promote(Mid)
-  println(promoted)
+  println(sample.map(mapToBoxChar))
+  println(sample)
