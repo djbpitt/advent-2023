f2ec0b6974e3a332ff1498aea8b3ecbfbe8197ee
David J Birnbaum <djbpitt@gmail.com>
2023-12-14 11:13:31 -0500

    12-11 Expand matrix fully
    TIL that transpose is predefined on nested sequences

--- src/main/scala/Advent_12-11.scala
+++ src/main/scala/Advent_12-11.scala
@@ -1,32 +1,26 @@
+package Advent_12_11
+
 import scala.annotation.tailrec
 import scala.io.Source
 
-private def expand(in: String): Vector[String] =
+private def expandMatrix(in: Vector[String]): String =
+  val result: String = in
+    .map(_.toVector)
+    .flatMap(expandRows) // = map and flatten
+    .transpose
+    .flatMap(expandRows)
+    .transpose // transpose again so that rows and columns are as in input
+    .map(_.mkString) // stringify rows
+    .mkString("\n") // stringify matrix
+  result
+
+private def expandRows(in: Vector[Char]): Vector[Vector[Char]] =
   in match
     case e if e.contains('#') => Vector(e)
     case e => Vector(e, e)
 
-private def transpose(in: Vector[String]): Vector[String] =
-  val lineCount = in.size
-  val lineLength = in.head.length
-  val transposed =
-    (for i <- 0 until lineLength yield
-      (for j <- 0 until lineCount yield
-        (in(j)(i))).mkString).toVector
-  transposed
-
 @main def main11(): Unit =
   val rawInput: Vector[String] = Source.fromResource("12-11_data_test.txt").getLines.toVector
-  val expandedRows = rawInput.map(expand).flatten
-  println("Expanded rows:")
-  println(expandedRows.mkString("\n"))
-  val transposed = transpose(expandedRows)
-  println("Transposed:")
-  println(transposed.mkString("\n"))
-  val expandedCols = transposed.map(expand).flatten
-  println("Expanded but still transposed:")
-  println(expandedCols.mkString("\n"))
-  val fullyExpanded = transpose(expandedCols)
-  println("Fully expanded and reoriented:")
-  println(fullyExpanded.mkString("\n"))
+  val expanded: String = expandMatrix(rawInput)
+
 
