cd0c0676a1b66a48352760ce3aa40b1c69f56f8d
David J Birnbaum <djbpitt@gmail.com>
2023-12-14 19:56:55 -0500

    12-11 Refactor expandRows and expandMatrix to remove specification of galaxy char
    If number of distinct chars in row is greater than 1, there must be a galaxy
    This approach won't work for part 2, where we have to count galaxies, and not just check for 1 or more

--- src/main/scala/Advent_12-11.scala
+++ src/main/scala/Advent_12-11.scala
@@ -14,12 +14,12 @@ import scala.io.Source
  * @param scale  number of copies to make if target not found as Int
  * @return expanded matrix as Vector[Vector[Char]]
  */
-private def expandMatrix(in: Vector[String], target: Char, scale: Int): Vector[Vector[Char]] =
+private def expandMatrix(in: Vector[String], scale: Int): Vector[Vector[Char]] =
   val result: Vector[Vector[Char]] = in
     .map(_.toVector)
-    .flatMap(e => expandRows(e, target, scale)) // = map and flatten
+    .flatMap(e => expandRows(e, scale)) // = map and flatten
     .transpose
-    .flatMap(e => expandRows(e, '#', 2))
+    .flatMap(e => expandRows(e, scale))
     .transpose // transpose again so that rows and columns are as in input
   result
 
@@ -32,10 +32,8 @@ private def expandMatrix(in: Vector[String], target: Char, scale: Int): Vector[V
  * @param scale  number of times to copy row as Int
  * @return the input plus an additional copy if the row contains no #
  */
-private def expandRows(in: Vector[Char], target: Char, scale: Int): Vector[Vector[Char]] =
-  in match
-    case e if e.contains(target) => Vector(e)
-    case e => Vector.fill(scale)(e)
+private def expandRows(in: Vector[Char], scale: Int): Vector[Vector[Char]] =
+  if in.distinct.size > 1 then Vector(in) else Vector.fill(scale)(in)
 
 /** Find all galaxies (# characters) in matrix and return (row, col)
  *
@@ -94,12 +92,18 @@ private def findAllGalaxyPairs(galaxies: Vector[(Int, Int)]) =
   result
 
 @main def main11(): Unit =
-  val rawInput: Vector[String] = Source.fromResource("12-11_data.txt").getLines.toVector
-  val expanded: Vector[Vector[Char]] = expandMatrix(rawInput, target = '#', scale = 2)
-  val galaxies: Vector[(Int, Int)] = findGalaxies(expanded)
-  val galaxyPairs: Vector[((Int, Int), (Int, Int))] = findAllGalaxyPairs(galaxies)
-  val distances: Vector[Int] = galaxyPairs.map(e => manhattan(e._1, e._2))
-  val result1 = distances.sum / 2 // Pairs contain (a,b) and (b, a) so divide by 2 to remove mirrors
+  val rawInput: Vector[String] = Source.fromResource("12-11_data_test.txt").getLines.toVector
+  val expanded1: Vector[Vector[Char]] = expandMatrix(rawInput, scale = 2)
+  println(expanded1.map(_.mkString).mkString("\n"))
+  val galaxies1: Vector[(Int, Int)] = findGalaxies(expanded1)
+  println(galaxies1)
+  val galaxyPairs1: Vector[((Int, Int), (Int, Int))] = findAllGalaxyPairs(galaxies1)
+  val distances1: Vector[Int] = galaxyPairs1.map(e => manhattan(e._1, e._2))
+  val result1 = distances1.sum / 2 // Pairs contain (a,b) and (b, a) so divide by 2 to remove mirrors
+  // Part 2 expands by 1_000_000
+  // Currently set for 2; heap overflow with 1_000_000
+
   println(s"Part 1 solution: $result1")
+
 
 
