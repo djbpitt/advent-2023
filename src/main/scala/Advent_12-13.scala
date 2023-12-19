98eb6d1006b12e71aac4baae7d3d9b64c4a2845b
David J Birnbaum <djbpitt@gmail.com>
2023-12-19 12:08:26 -0500

    12-13 Plan algorithm, verify transposition

--- src/main/scala/Advent_12-13.scala
+++ src/main/scala/Advent_12-13.scala
@@ -12,6 +12,21 @@ import scala.util.matching.Regex
     .getLines()
     .mkString("\n")
   val tasks = rawLines.split("\n\n").toVector.map(_.split("\n").map(_.toVector).toVector)
+  /* In both original and transposed form:
+  *
+  * 1. Insert spacers to force all row and column counts to be odd
+  * 2. Shape into vector of strings
+  * 3. Find all palindromes (and their lengths) in all strings
+  * 4. Find all aligned palindrome centers (non-zero values array of lengths)
+  * 5. Find all shared alignments and select the longest (?)
+  * 6. Adjust for insertions
+  * 7. Value is sum of number of rows above center in original
+  *    and 10 times the number of rows above center in transposed
+  * */
+  println("Original")
+  tasks.head.foreach(println)
+  println("Transposed")
+  tasks.head.transpose.foreach(println)
 
 
 
